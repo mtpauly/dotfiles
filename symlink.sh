1980e9e2c64d9d0afc2e0a97e1ac5bd1dd4e490d
mtpauly <mpauly@college.harvard.edu>
2023-06-20 23:21:02 -0400

    Remove p10k from symlink

--- symlink.sh
+++ symlink.sh
@@ -5,7 +5,6 @@ ln -sf $HOME/.dotfiles/.zshrc $HOME/.zshrc
 ln -sf $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
 ln -sf $HOME/.dotfiles/.yabairc $HOME/.yabairc
 ln -sf $HOME/.dotfiles/.skhdrc $HOME/.skhdrc
-ln -sf $HOME/.dotfiles/.p10k.zsh $HOME/.p10k.zsh
 
 # .config
 ln -sf $HOME/.dotfiles/nvim $HOME/.config
