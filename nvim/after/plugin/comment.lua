f0b98c9631a55ff12579965bada7eb4e8a2f2845
mtpauly <mpauly@college.harvard.edu>
2023-07-04 13:52:14 -0400

    Add Comment.nvim plugin

---
+++ nvim/after/plugin/comment.lua
@@ -0,0 +1,15 @@
+require('Comment').setup({
+    toggler = {
+        line = '<leader>cc',
+        block = '<leader>bc',
+    },
+    opleader = {
+        line = '<leader>c',
+        block = '<leader>b',
+    },
+    mappings = {
+        basic = true,
+        extra = false,
+    },
+})
+
