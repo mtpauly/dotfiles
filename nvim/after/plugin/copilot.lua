fc566430886b60d50a855d773de0672fb61e7367
mtpauly <mpauly@college.harvard.edu>
2023-10-06 16:14:24 -0400

    Renable copilot

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -1,40 +1,41 @@
-vim.g.copilot_no_tab_map = true
-vim.g.copilot_assume_mapped = true
-vim.g.copilot_tab_fallback = ""
+require("copilot").setup({
+    panel = {
+        enabled = true,
+        auto_refresh = true,
+        keymap = {
+            jump_prev = "<C-p>",
+            jump_next = "<C-n>",
+            accept = "<C-y>",
+        },
+        layout = {
+            position = "right",
+        },
+    },
+    suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+            prev = "<C-p>",
+            next = "<C-n>",
+            accept = "<C-y>",
+        },
+    },
+    filetypes = {
+        yaml = true,
+        markdown = true,
+        help = true,
+        gitcommit = true,
+        gitrebase = true,
+        hgcommit = true,
+        svn = true,
+        cvs = true,
+        ["."] = true,
+    },
+})
 
-vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.cmd("silent Copilot disable") -- start disabled
 
--- TODO: this is not correctly disabling tab
--- possible is has something to do with tab not being unbound in completions
-
--- start disabled
--- TODO:
-
--- vim.api.nvim_set_keymap(
---     "n",
---     "<space>cs",
---     ":Copilot status<CR>",
---     { noremap = true }
--- )
---
--- vim.api.nvim_set_keymap(
---     "n",
---     "<space>cp",
---     ":Copilot panel<CR>",
---     { noremap = true }
--- )
---
--- vim.api.nvim_set_keymap(
---     "n",
---     "<space>cd",
---     ":Copilot disable<CR>",
---     { noremap = true }
--- )
---
--- vim.api.nvim_set_keymap(
---     "n",
---     "<space>ce",
---     ":Copilot enable<CR>",
---     { noremap = true }
--- )
-
+vim.api.nvim_set_keymap("n", "<space>cs", ":Copilot status<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<space>cp", ":Copilot panel<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<space>cd", ":Copilot disable<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<space>ce", ":Copilot enable<CR>", { noremap = true })
