2083a3a42b55806a6dcfc2edbea862a5b3fbfe12
mtpauly <mpauly@college.harvard.edu>
2023-12-18 23:32:03 -0600

    Use treesitter for indentation

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -7,5 +7,10 @@ require'nvim-treesitter.configs'.setup {
         enable = true,
         additional_vim_regex_highlighting = false,
     },
+
+    indent = {
+        enable = true,
+        -- disable = { "python", },
+    },
 }
 
