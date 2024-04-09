2132de1ab5c696d306c3bd438f2cd187a8ba2dfd
mtpauly <mpauly@college.harvard.edu>
2024-04-09 19:51:11 -0400

    Update harpoon to version 2

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,28 +1,11 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local harpoon = require("harpoon")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
+harpoon:setup()
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
--- TODO: figure out how to get this to work
-vim.keymap.set("n", "<C-1>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-2>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-3>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-4>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-5>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-6>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-7>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
--- TODO: need to rebind draw screen to <leader> <C-l>
--- or do this through tmux (?)
-
-require("harpoon").setup({
-    menu = {
-        width = vim.api.nvim_win_get_width(0) - 8,
-    }
-})
-
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
