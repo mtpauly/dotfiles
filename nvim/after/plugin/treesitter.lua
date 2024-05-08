bd76b3c8310b5a7d2bab5259a3563172f8027f18
mtpauly <mpauly@college.harvard.edu>
2024-05-08 11:29:47 -0400

    Remove treesitter indentation

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -7,10 +7,5 @@ require'nvim-treesitter.configs'.setup {
         enable = true,
         additional_vim_regex_highlighting = false,
     },
-
-    indent = {
-        enable = true,
-        -- disable = { "python", },
-    },
 }
 
