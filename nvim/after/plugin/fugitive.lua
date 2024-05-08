daa91a42088159b59572b8df9ed8d75e4e8584c4
mtpauly <mpauly@college.harvard.edu>
2024-05-08 15:46:55 -0400

    Improve git fugitive keybinds

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,4 +1,12 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>gb", '<cmd>G blame<cr>')
-vim.keymap.set("n", "<leader>gd", '<cmd>G diff<cr>')
+local opts = { noremap = true }
 
+vim.keymap.set('n', '<leader>gs', '<cmd>0G<cr>', opts)
+vim.keymap.set('n', '<leader>gb', '<cmd>G blame<cr>', opts)
+vim.keymap.set('n', '<leader>gdd', '<cmd>0G diff<cr>', opts)
+vim.keymap.set('n', '<leader>gds', '<cmd>0G diff --staged<cr>', opts)
+vim.keymap.set('n', '<leader>gdv', '<cmd>Gvdiffsplit<cr>', opts)
+vim.keymap.set('n', '<leader>gdh', '<cmd>Ghdiffsplit<cr>', opts)
+vim.keymap.set('n', '<leader>gl', '<cmd>0G log<cr>', opts)
+vim.keymap.set('n', '<leader>gp', '<cmd>0G log -p --max-count=10<cr>', opts)
+vim.keymap.set('n', '<leader>gf', '<cmd>0G log %<cr>', opts)
+vim.keymap.set('n', '<leader>ga', '<cmd>0G branch -a', opts)
