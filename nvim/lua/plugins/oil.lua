d09a2ae9157ad1930ab0f0dd9b415f9f409037d2
mtpauly <peregrine.pauly@gmail.com>
2025-04-20 12:55:53 -0700

    add oil nvim

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,14 @@
+return {
+  'stevearc/oil.nvim',
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  lazy = false,
+  config = function()
+    require'oil'.setup {
+      view_options = {
+	show_hidden = true,
+      },
+    }
+
+    vim.keymap.set('n', '<leader>o', '<cmd>Oil<cr>')
+  end,
+}
