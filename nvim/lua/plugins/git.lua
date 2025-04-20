b1a96514f5dff1ec280771b05517128cad928eed
mtpauly <peregrine.pauly@gmail.com>
2025-04-20 14:45:55 -0700

    Git signs

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -7,11 +7,23 @@ return {
       vim.keymap.set('n', '<leader>gd', '<cmd>Gvdiffsplit<cr>', { noremap = true })
       vim.keymap.set('n', '<leader>gl', '<cmd>0G log --stat --max-count=100<cr>', { noremap = true })
       vim.keymap.set('n', '<leader>gf', '<cmd>0G log -p --max-count=100 %<cr>', { noremap = true })
-      vim.keymap.set('x', '<leader>go', '<cmd>.GBrowse!<cr>', opts)
+      vim.keymap.set('x', '<leader>go', '<cmd>.GBrowse!<cr>', { noremap = true })
     end,
   },
   { 'tpope/vim-rhubarb' },
-  { 'tpope/vim-surround' },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup({
+        signs = {
+          add = { text = '+' },
+          change = { text = '~' },
+          delete = { text = '-' },
+          topdelete = { text = '-' },
+          changedelete = { text = '~' },
+          untracked = { text = '+' },
+        },
+      })
+    end
+  },
 }
-
-
