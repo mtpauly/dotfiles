a8ad91fb152079aefbc7dad6668ca3ea383a80b6
mtpauly <peregrine.pauly@gmail.com>
2025-04-23 17:11:48 -0700

    Fix multigrep formatting

--- nvim/lua/config/telescope/multigrep.lua
+++ nvim/lua/config/telescope/multigrep.lua
@@ -10,25 +10,25 @@ return function(opts)
   local finder = finders.new_async_job {
     command_generator = function(prompt)
       if not prompt or prompt == "" then
-	return nil
+        return nil
       end
 
       local pieces = vim.split(prompt, "  ")
       local args = { "rg" }
       if pieces[1] then
-	table.insert(args, "-e")
-	table.insert(args, pieces[1])
+        table.insert(args, "-e")
+        table.insert(args, pieces[1])
       end
       -- TODO: make fuzzy?
 
       if pieces[2] then
-	table.insert(args, "-g")
-	table.insert(args, pieces[2])
+        table.insert(args, "-g")
+        table.insert(args, pieces[2])
       end
 
       return vim.iter({
-	args,
-	{ '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case' },
+        args,
+        { '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case' },
       }):flatten():totable()
     end,
     entry_maker = make_entry.gen_from_vimgrep(opts),
