de8d1dcbd9fab0b5f066d6e1f6186568abdbe659
mtpauly <peregrine.pauly@gmail.com>
2025-05-02 16:34:32 -0700

    Add lsp keybinds

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -21,8 +21,18 @@ return {
         pyright = {}, -- https://github.com/microsoft/pyright/blob/main/docs/installation.md
       }
 
+      local on_attach = function(client, bufnr)
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
+      end
+
       for server, config in pairs(servers) do
         config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        config.on_attach = on_attach
         require'lspconfig'[server].setup(config)
       end
     end,
