4c8f52af7932eede2afab4c4b54ebdc228e34f7f
mtpauly <peregrine.pauly@gmail.com>
2025-06-06 12:04:13 -0700

    tokyonight dark background

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,21 @@ return {
   priority = 1000,
   opts = {},
   config = function()
+    require("tokyonight").setup({
+      style = "moon",
+      on_colors = function(colors)
+        colors.bg = "#000000"
+        colors.bg_dark = "#000000"
+        colors.bg_dark1 = "#000000"
+        colors.bg_float = "#000000"
+        -- colors.bg_highlight = "#000000"
+        colors.bg_popup = "#000000"
+        -- colors.bg_search = "#000000"
+        colors.bg_sidebar = "#000000"
+        colors.bg_statusline = "#000000"
+        -- colors.bg_visual = "#000000"
+      end,
+    })
     vim.cmd[[colorscheme tokyonight]]
   end
 }
