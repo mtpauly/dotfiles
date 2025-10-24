b8998bd25e840cf6578f7b75fe05bb02639e4f1f
mtpauly <peregrine.pauly@gmail.com>
2025-10-24 10:55:25 -0700

    nvim: Add execute lua code mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -96,5 +96,10 @@ end, { noremap = true, silent = true })
 vim.keymap.set("n", "<c-n>", "<cmd>cnext<CR>", { noremap = true })
 vim.keymap.set("n", "<c-p>", "<cmd>cprev<CR>", { noremap = true })
 
+-- Execute lua code
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
 -- Source plugins
 require('config.lazy')
