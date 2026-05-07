c4c0de2885897fb6cc6c96684e8ac2107c5ea870
mtpauly <peregrine.pauly@gmail.com>
2026-05-07 02:21:33 +0000

    Update telescope to fix compat issue with new treesitter

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  'nvim-telescope/telescope.nvim', tag = 'v0.1.9',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
